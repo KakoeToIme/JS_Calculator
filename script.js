cda1f3be607ff06155bc330a4a48a96d194c6b1f
rusanov_kirill <set.ds.eu@gmail.com>
2024-09-09 19:10:07 +0600

    Plus and Equal operators have added

--- script.js
+++ script.js
@@ -2,26 +2,61 @@
 
 const displayNum = val => {
     let allowEdit = true;
-    const displayVal = document.getElementsByClassName('numpad');
+    const displayVal = document.getElementsByClassName('numpadOne');
 
     if (displayVal[0].textContent.length > 15) allowEdit = false;
     if (displayVal[0].textContent.length <= 15) allowEdit = true;
 
-    if (displayVal[0].textContent !== '0' && allowEdit === true) document.getElementsByClassName('numpad')[0].textContent += val;
+    if (displayVal[0].textContent !== '0' && allowEdit === true) document.getElementsByClassName('numpadOne')[0].textContent += val;
     else if (allowEdit === true) {
-        document.getElementsByClassName('numpad')[0].textContent = val;
+        document.getElementsByClassName('numpadOne')[0].textContent = val;
         document.getElementById('clear').textContent = 'C';
     }
 }
 
 const AC = () => {
-    document.getElementsByClassName('numpad')[0].textContent = 0;
+    document.getElementsByClassName('numpadOne')[0].textContent = 0;
+    document.getElementsByClassName('numpadTwo')[0].textContent = '';
+    document.getElementsByClassName('operand')[0].textContent = '';
     document.getElementById('clear').textContent = 'AC';
 }
 
 const DEL = () => {
-    const newText = document.getElementsByClassName('numpad')[0].textContent.slice(0, -1);
+    const newText = document.getElementsByClassName('numpadOne')[0].textContent.slice(0, -1);
 
-    if (document.getElementsByClassName('numpad')[0].textContent.length > 1) document.getElementsByClassName('numpad')[0].textContent = newText;
-    else document.getElementsByClassName('numpad')[0].textContent = '0';
+    if (document.getElementsByClassName('numpadOne')[0].textContent.length > 1) document.getElementsByClassName('numpadOne')[0].textContent = newText;
+    else {
+        document.getElementsByClassName('numpadOne')[0].textContent = '0';
+        document.getElementById('clear').textContent = 'AC';
+    }
+}
+
+const PLUS = () => {
+    let temp = 0;
+    const firstNum = document.getElementsByClassName('numpadOne')[0].textContent;
+
+    if (document.getElementsByClassName('numpadTwo')[0].textContent === '') {
+        document.getElementsByClassName('numpadTwo')[0].textContent = firstNum;
+        document.getElementsByClassName('operand')[0].textContent = '+';
+        document.getElementsByClassName('numpadOne')[0].textContent = '0';
+    }
+    else {
+        temp = Number(firstNum) + Number(document.getElementsByClassName('numpadTwo')[0].textContent);
+        document.getElementsByClassName('numpadTwo')[0].textContent = temp;
+        document.getElementsByClassName('operand')[0].textContent = '+';
+        document.getElementsByClassName('numpadOne')[0].textContent = '0';
+    }
+}
+
+const EQUAL = () => {
+    const firstNum = document.getElementsByClassName('numpadOne')[0].textContent;
+    const secondNum = document.getElementsByClassName('numpadTwo')[0].textContent;
+
+    switch (document.getElementsByClassName('operand')[0].textContent) {
+        case '+':
+            document.getElementsByClassName('numpadOne')[0].textContent = Number(firstNum) + Number(secondNum);
+            document.getElementsByClassName('numpadTwo')[0].textContent = '';
+            document.getElementsByClassName('operand')[0].textContent = '';
+            break;
+    }
 }
